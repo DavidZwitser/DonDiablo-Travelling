6d557a9f90cd29d0f7439c7ba780e9c55b28bdf8
DavidZwitser <davidzwitser@gmail.com>
2018-04-16 14:47:49 +0200

    Added the untested code for the perspective renderer

--- ts/Rendering/PerspectiveRenderer.ts
+++ ts/Rendering/PerspectiveRenderer.ts
@@ -1,12 +1,51 @@
 import Renderer from './Renderer';
 import PerspectiveObject from './Sprites/PerspectiveObject';
 
+interface IScreenTransform
+{
+    x: number;
+    y: number;
+    scale: number;
+}
+
 /** Renders sprites in a pseudo 3d way */
 export default class PerspectiveRenderer extends Renderer<PerspectiveObject>
 {
+    private _horizonPoint: Phaser.Point;
+    private _depthOfField: number;
+
+    constructor(game: Phaser.Game)
+    {
+        super(game);
+
+        this._depthOfField = .96;
+        this._horizonPoint = new Phaser.Point(.5, .45);
+    }
+
     /** Render the sprites in pseudo3d way */
     public render(): void
     {
-        //
+        this.forEachObject(this.eachObject, this);
+    }
+
+    private eachObject(object: PerspectiveObject): void
+    {
+        let targetTransform: IScreenTransform = this.screenToWorldPosition(object.xPos, object.zPos);
+    }
+
+    public screenToWorldPosition(xPos: number, zPos: number): IScreenTransform
+    {
+
+        let perspectiveOffset: number = this._horizonPoint.y / zPos;
+
+        let projectedX: number = (this._horizonPoint.x + xPos * perspectiveOffset) * this.game.width;
+        let projectedY: number = (this._horizonPoint.y + (1 - this._horizonPoint.y) * perspectiveOffset) * this.game.height;
+        let projectedScale: number = perspectiveOffset;
+
+        return {
+            x: projectedX,
+            y: projectedY,
+            scale: projectedScale
+        };
     }
 }
