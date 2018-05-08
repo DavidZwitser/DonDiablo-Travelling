a1fc7c859fa0568dcadb90d0dfda48387c9f2722
FerryElbaghdadi <18839@ma-web.nl>
2018-05-08 11:44:19 +0200

    Added Ability to Switch between Animations for Player

--- ts/GameObjects/Interactable/Perspective/Player.ts
+++ ts/GameObjects/Interactable/Perspective/Player.ts
@@ -1,17 +1,61 @@
 import { Lanes } from '../../../Enums/Lanes';
 import ReactivePerspectiveObject from '../../../Rendering/Sprites/ReactivePerspectiveObject';
 
-
 import AtlasImages from '../../../Data/Atlases';
 
 /** The player controlled by the user */
 export default class Player extends ReactivePerspectiveObject implements Phaser.Sprite
 {
     private _currentLane: Lanes;
+
     private _playerSprite: Phaser.Sprite;
 
+    public spine: any;
     public speed: number;
     public collectedPickup: Phaser.Signal;
+
+    public static ANIMATION_DRIVE: string = 'drive';
+    public static ANIMATION_TURN: string = 'turn';
+    public static ANIMATION_LOSE: string = 'defeat';
+
+    constructor(game: Phaser.Game)
+    {
+       super(game);
+
+       this._playerSprite = this.game.add.sprite(this.game.width / 3, this.game.height / 1.25, AtlasImages.Interface, 'Spacecraft_Main');
+
+       this.spine = new PhaserSpine.Spine(<PhaserSpine.SpineGame>(this.game), 'Character');
+      // this.addChild(this.spine);
+
+      /*
+        SPINE / ANIMATIONS TEMPORARILY DISABLED.
+        NO ANIMATIONS AVAILABLE
+      */
+    }
+
+    private setAnimation(animation: string, loop: boolean = false): void
+    {
+        this.spine.setAnimationByName(0, animation, loop);
+
+        this.spine.onComplete.addOnce( () => { if (!loop) {
+            this.setAnimation(Player.ANIMATION_DRIVE, true);
+        }});
+    }
+
+    public turn(): void
+    {
+        this.setAnimation(Player.ANIMATION_TURN, false);
+    }
+
+    public lose(): void
+    {
+        this.setAnimation(Player.ANIMATION_LOSE, false);
+    }
+
+    public pause( pause: boolean): void
+    {
+        this.spine.autoUpdate = !pause;
+    }
 
     public changeLane( lane: Lanes ): void
     {
@@ -25,9 +69,12 @@ export default class Player extends ReactivePerspectiveObject implements Phaser.
         //
     }
 
-    constructor(game: Phaser.Game)
+    public destroy(): void
     {
-       super(game);
-       this._playerSprite = this.game.add.sprite(this.game.width / 3, this.game.height / 1.25, AtlasImages.Interface, 'Spacecraft_Main');
+        super.destroy(true);
+
+        if (this.spine) { this.spine.destroy(true); }
+        this.spine = null;
     }
+
 }
