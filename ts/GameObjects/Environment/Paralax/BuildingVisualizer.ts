ff3656f418c9188523566148964a5dc7e43e2edb
nathannieuwenhuizen <nathan-san@live.nl>
2018-05-16 14:28:10 +0200

    buildings in visualizers have correct sizes on all aspect ratios

--- ts/GameObjects/Environment/Paralax/BuildingVisualizer.ts
+++ ts/GameObjects/Environment/Paralax/BuildingVisualizer.ts
@@ -6,8 +6,8 @@ import Atlases from '../../../Data/Atlases';
 /** Visualizes music */
 export default class BuildingVisualizer extends Phaser.Group
 {
-    private maxWidth: number;
-    private maxHeight: number;
+    private _maxWidth: number;
+    private _maxHeight: number;
 
     private _barDistance: number = 1;
     private _contextAvailable: boolean;
@@ -29,15 +29,16 @@ export default class BuildingVisualizer extends Phaser.Group
 
     private _maskGraphic: Phaser.Graphics;
 
-    constructor (game: Phaser.Game, maxWidth: number, maxHeight: number) {
+    constructor (game: Phaser.Game, maxWidth: number, maxHeight: number)
+    {
         super(game);
 
         this._buildings = [];
         this._topBuildings = [];
 
         this._contextAvailable = AudioAnalyser.getInstance().Setup();
-        this.maxWidth = maxWidth;
-        this.maxHeight = maxHeight;
+        this._maxWidth = maxWidth;
+        this._maxHeight = maxHeight;
 
         this._topHalf = new Phaser.Group(game);
         this.addChild(this._topHalf);
@@ -49,7 +50,7 @@ export default class BuildingVisualizer extends Phaser.Group
         this._maskGraphic = new Phaser.Graphics(this.game, 0, 0);
         this.addChild(this._maskGraphic);
         this._maskGraphic.beginFill(0xFF3300);
-        this._maskGraphic.drawRect(0, -this.maxHeight, this.maxWidth, maxHeight);
+        this._maskGraphic.drawRect(0, -this._maxHeight, this._maxWidth, maxHeight);
         this._maskGraphic.endFill();
 
         this.setUp();
@@ -57,7 +58,8 @@ export default class BuildingVisualizer extends Phaser.Group
 
         this.mask = this._maskGraphic;
     }
-    private setUp(): void {
+    private setUp(): void
+    {
         //bottom half
         this._backGlow = new Phaser.Sprite(this.game, 0, 0, Atlases.Interface, 'Purple_glow_utopia');
         this._backGlow.anchor.set(0, 1);
@@ -79,23 +81,26 @@ export default class BuildingVisualizer extends Phaser.Group
         this._topGlow = new Phaser.Sprite(this.game, 0, 0, Atlases.Interface, 'Upper_glow_utopia');
         this._topGlow.anchor.set(0, 1);
         this._topHalf.addChild(this._topGlow);
-        this._topHalf.y = -this.maxHeight * 2;
+        this._topHalf.y = -this._maxHeight * 2;
         this._topHalf.scale.set(1, -1);
 
     }
 
-    private setUpBuildings(top: boolean): void {
+    private setUpBuildings(top: boolean): void
+    {
         let pos: number = 0;
-        while (pos < this.maxWidth) {
+        while (pos < this._maxWidth) {
             let index: number =  Math.ceil(Math.random() * 14);
             let building: Phaser.Sprite = new Phaser.Sprite(this.game, pos, 0, Atlases.Interface, 'Background_Building_Blue_' + (index > 9 ? index : ('0' + index)));
             building.anchor.set(0);
-            building.scale.set(.5);
+            building.scale.set((this.game.width / GAME_WIDTH) * .5);
 
-            if (top) {
+            if (top)
+            {
                 this._topHalf.addChild(building);
                 this._topBuildings.push(building);
-            } else {
+            } else
+            {
                 this._bottomHalf.addChild(building);
                 this._buildings.push(building);
             }
@@ -108,10 +113,13 @@ export default class BuildingVisualizer extends Phaser.Group
     public render(): void
     {
         //when context isn't available
-        if (!this._contextAvailable) {
-            if (!this._topIsActive) {
+        if (!this._contextAvailable)
+        {
+            if (!this._topIsActive)
+            {
                 this.renderBuildingWithoutContext(this._buildings, this._glow, this._backGlow);
-            } else {
+            } else
+            {
                 this.renderBuildingWithoutContext(this._topBuildings, this._topGlow, this._topBackGlow);
             }
             return;
@@ -121,15 +129,19 @@ export default class BuildingVisualizer extends Phaser.Group
         AudioAnalyser.getInstance().analyser.getByteFrequencyData(AudioAnalyser.getInstance().dataArray);
 
         //update wther one half is active
-        if (!this._topIsActive) {
+        if (!this._topIsActive)
+        {
             this.renderBuilding(this._buildings, this._glow, this._backGlow);
-        } else {
+        } else
+        {
             this.renderBuilding(this._topBuildings, this._topGlow, this._topBackGlow);
         }
     }
 
-    private renderBuildingWithoutContext(array: Phaser.Sprite[], glow: Phaser.Sprite, backGlow: Phaser.Sprite): void {
-        for (let i: number = Math.floor(array.length / 2); i--;) {
+    private renderBuildingWithoutContext(array: Phaser.Sprite[], glow: Phaser.Sprite, backGlow: Phaser.Sprite): void
+    {
+        for (let i: number = Math.floor(array.length / 2); i--;)
+        {
             let precentage: number = (1 - i / (array.length / 2));
             let height: number = array[i].height * precentage;
             array[i].y = -height;
@@ -138,9 +150,12 @@ export default class BuildingVisualizer extends Phaser.Group
         }
         glow.alpha = backGlow.alpha = -array[1].y / array[1].height;
     }
-    private renderBuilding(array: Phaser.Sprite[], glow: Phaser.Sprite, backGlow: Phaser.Sprite): void {
-        for (let i: number = Math.floor(array.length / 2); i--;) {
-            let precentage: number = AudioAnalyser.getInstance().dataArray[Math.round(array[i].x / this.maxWidth * AudioAnalyser.getInstance().bufferLength * 2)] / 256;
+
+    private renderBuilding(array: Phaser.Sprite[], glow: Phaser.Sprite, backGlow: Phaser.Sprite): void
+    {
+        for (let i: number = Math.floor(array.length / 2); i--;)
+        {
+            let precentage: number = AudioAnalyser.getInstance().dataArray[Math.round(array[i].x / this._maxWidth * AudioAnalyser.getInstance().bufferLength * 2)] / 256;
             let height: number = precentage * array[i].height;
             array[i].y = -height;
             height = precentage * array[array.length - 1 - i].height;
@@ -149,21 +164,26 @@ export default class BuildingVisualizer extends Phaser.Group
         glow.alpha = backGlow.alpha = -array[1].y / array[1].height;
     }
 
-    public switch(): void {
+    public switch(): void
+    {
         this._topIsActive = !this._topIsActive;
         this.deactivateHalf();
     }
-    private deactivateHalf(): void {
-        if (!this._topIsActive) {
-            this.game.add.tween(this._topHalf).to({y: -this.maxHeight * 2}, this._switchTime, Phaser.Easing.Cubic.InOut, true);
+    private deactivateHalf(): void
+    {
+        if (!this._topIsActive)
+        {
+            this.game.add.tween(this._topHalf).to({y: -this._maxHeight * 2}, this._switchTime, Phaser.Easing.Cubic.InOut, true);
             this.game.add.tween(this._bottomHalf).to({y: 0}, this._switchTime, Phaser.Easing.Cubic.InOut, true);
-        } else {
-            this.game.add.tween(this._topHalf).to({y: -this.maxHeight}, this._switchTime, Phaser.Easing.Cubic.InOut, true);
-            this.game.add.tween(this._bottomHalf).to({y: this.maxHeight}, this._switchTime, Phaser.Easing.Cubic.InOut, true);
+        } else
+        {
+            this.game.add.tween(this._topHalf).to({y: -this._maxHeight}, this._switchTime, Phaser.Easing.Cubic.InOut, true);
+            this.game.add.tween(this._bottomHalf).to({y: this._maxHeight}, this._switchTime, Phaser.Easing.Cubic.InOut, true);
         }
     }
 
-    public resize(): void {
+    public resize(): void
+    {
         //needs more things!
         this.y = this.game.height * .6;
         requestAnimationFrame(() => {
@@ -171,7 +191,8 @@ export default class BuildingVisualizer extends Phaser.Group
         });
     }
 
-    public destroy(): void {
+    public destroy(): void
+    {
         super.destroy(true);
     }
 }
